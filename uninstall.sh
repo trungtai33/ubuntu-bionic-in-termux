a8fcedc73200e9f79298c0af621d83f401012b90
Nguyễn Trung Tài <83492345+trungtai33@users.noreply.github.com>
2021-10-05 19:36:37 +0700

    official

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
-if [ -d "$PREFIX/share/ubuntu-bionic" ]; then
-printf "\n\e[34m[\e[32m*\e[34m]\e[36m Uninstalling Ubuntu Bionic, please wait...\n\e[31m"
-rm -rf "$PREFIX/share/ubuntu-bionic"
-rm -f "$PREFIX/bin/start-ubuntu-bionic"
+distro_name="Ubuntu Bionic"
+if [ -d "${PREFIX}/share/ubuntu-bionic" ]; then
+printf "\n\e[34m[\e[32m*\e[34m]\e[36m Uninstalling ${distro_name}, please wait...\n\e[31m"
+rm -rf "${PREFIX}/share/ubuntu-bionic"
+rm -f "${PREFIX}/bin/start-ubuntu-bionic"
 printf "\e[34m[\e[32m*\e[34m]\e[36m Uninstall successfully.\n\n\e[0m"
 exit
 fi
-printf "\n\e[31mError: distribution Ubuntu Bionic is not installed.\n\n\e[0m"
+printf "\n\e[31mError: distribution ${distro_name} is not installed.\n\n\e[0m"
