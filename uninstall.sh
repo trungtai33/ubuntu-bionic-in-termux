3db3ddf4e2f6758cc601fbb5aaae3c7e37d1a074
Nguyễn Trung Tài <83492345+trungtai33@users.noreply.github.com>
2021-10-06 07:05:06 +0700

    official

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
+directory="ubuntu-bionic"
 distro_name="Ubuntu Bionic"
-if [ -d "${PREFIX}/share/ubuntu-bionic" ]; then
+if [ -d "${PREFIX}/share/${directory}" ]; then
 printf "\n\e[34m[\e[32m*\e[34m]\e[36m Uninstalling ${distro_name}, please wait...\n\e[31m"
-rm -rf "${PREFIX}/share/ubuntu-bionic"
-rm -f "${PREFIX}/bin/start-ubuntu-bionic"
+rm -rf "${PREFIX}/share/${directory}"
+rm -f "${PREFIX}/bin/start-${directory}"
 printf "\e[34m[\e[32m*\e[34m]\e[36m Uninstall successfully.\n\n\e[0m"
 exit
 fi
